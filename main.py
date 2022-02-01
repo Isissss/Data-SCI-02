147f17a1462fff96e1b82d94e87e5c6502b1f2e1
Isis <89838626+Isissss@users.noreply.github.com>
2022-02-01 12:51:55 +0100

    Create main.py
    Project

---
+++ main.py
@@ -0,0 +1,73 @@
+import requests
+import time
+import Adafruit_DHT as dht
+import datetime 
+import RPi.GPIO as GPIO
+
+# Initialize the sensor with GPIO 13
+DHT_SENSOR = dht.DHT11
+DHT_PIN = 13
+
+#API Key for ThingSpeak needed for update call.
+api_key='MJES7KJYQ9M0QL4E'
+url = 'https://api.thingspeak.com/update.json'
+
+# Set GPIO mode to use GPIO names rather than absolute pin number
+GPIO.setmode(GPIO.BCM)
+#Disable warnings for now
+GPIO.setwarnings(False)
+
+#Define outputs
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+
+
+def measure():
+    while True:
+        # Define humidity and temperature variables
+        humidity, temperature = dht.read(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None and humidity < 100:
+            status = indicator(humidity); 
+            temperature_f = temperature * 9 / 5 + 32;
+            queries = {'api_key': api_key,'field2': humidity, 'field3' : temperature, 'field6' : status, 'field5' : temperature_f}
+            r = requests.get(url, params=queries)
+            print ("Temperature(C) = {0:0.1f}C  Humidity = {1:0.1f}% Temperature(F)= {2:0.1f}F ".format(temperature, humidity, temperature_f))
+            # Print result. If failed, result will return 0
+            print ("Result: ", r.text)
+            print (status)
+            # Light up the LED status indicator based on humidity value
+            GPIO.output(18,GPIO.HIGH);
+            time.sleep(15)
+        else:
+           print ("Sensor error!")
+           GPIO.output(18,GPIO.LOW);
+           time.sleep(3)
+                
+def indicator(humidity): 
+    if humidity is not None:
+        # Bad status
+        if humidity > 45.0:
+          status = 3
+          GPIO.output(24,GPIO.HIGH);
+          GPIO.output(23,GPIO.LOW);
+          GPIO.output(21,GPIO.LOW);
+          return status
+        # Orange value 
+        elif humidity > 35.0:  
+          status = 2
+          GPIO.output(23,GPIO.HIGH);
+          GPIO.output(21,GPIO.LOW);
+          GPIO.output(24,GPIO.LOW);
+          return status
+        # Good status
+        elif humidity > 31.0:
+          status = 1
+          GPIO.output(21,GPIO.HIGH);
+          GPIO.output(23,GPIO.LOW);
+          GPIO.output(24,GPIO.LOW);
+          return status
+        
+if __name__ == '__main__':
+    measure()
